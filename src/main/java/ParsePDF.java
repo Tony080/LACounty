2e90e22a3a6b8ff1b98786b1111d3af460eb4127
Tony <tony.hu1213@gmail.com>
2018-10-27 12:37:38 -0700

    Update template

--- src/main/java/ParsePDF.java
+++ src/main/java/ParsePDF.java
@@ -1,13 +1,11 @@
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.pdmodel.PDDocumentCatalog;
-import org.apache.pdfbox.pdmodel.interactive.annotation.PDAnnotationWidget;
 import org.apache.pdfbox.pdmodel.interactive.form.PDAcroForm;
 import org.apache.pdfbox.pdmodel.interactive.form.PDField;
 import org.apache.pdfbox.pdmodel.interactive.form.PDFieldTree;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.List;
 
 public class ParsePDF {
 
@@ -18,18 +16,30 @@ public class ParsePDF {
 
   public void openPdf(String fileName) throws IOException {
     PDDocument pdf = PDDocument.load(new File(fileName));
-    setField(pdf, "NAME", "dummy");
+    setField(pdf, "", "dummy");
+    pdf.setAllSecurityToBeRemoved(true);
+    pdf.save("cr180 result.pdf");
   }
 
   public void setField(PDDocument pdf, String name, String value) throws IOException {
     PDDocumentCatalog documentCatalog = pdf.getDocumentCatalog();
     PDAcroForm acroForm = documentCatalog.getAcroForm();
+    //Acquire all possible names
     PDFieldTree tree = acroForm.getFieldTree();
+    for (PDField aTree : tree) {
+      System.out.println(aTree);
+    }
+    //Set specified field with given name
+    //Firm name
+    PDField field = acroForm.getField("topmostSubform[0].Page1[0].Caption_sf[0].AttyInfo[0].AttyFirm_ft[0]");
+    //Case number
+    PDField field2 = acroForm.getField("topmostSubform[0].Page1[0].Caption_sf[0].CaseNumber[0].CaseNumber_ft[0]");
+    field2.setValue("99998");
 
-//    if (field != null) {
-//      field.setValue(value);
-//    } else {
-//      System.out.println("No such field!");
-//    }
+    if (field != null) {
+      field.setValue(value);
+    } else {
+      System.out.println("No such field!");
+    }
   }
 }
