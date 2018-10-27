eede1d0a432402f6eeece215dfeb9a70dd9b7fa8
Tony <tony.hu1213@gmail.com>
2018-10-27 12:59:14 -0700

    Update DefendantInfo, way to read & fill pdf

---
+++ src/main/java/AbstractPdfFiller.java
@@ -0,0 +1,3 @@
+interface AbstractPdfFiller {
+  void fillForm(DefendantInfo individualInfo);
+}
---
+++ src/main/java/DefendantInfo.java
@@ -0,0 +1,83 @@
+public class DefendantInfo {
+  private String firstName;
+  private String lastName;
+  private String caseNumber;
+  private String city;
+  private String state;
+  private String zip;
+  private String address;
+  private String telephone;
+  private String email;
+
+  public String getFirstName() {
+    return firstName;
+  }
+
+  public void setFirstName(String firstName) {
+    this.firstName = firstName;
+  }
+
+  public String getLastName() {
+    return lastName;
+  }
+
+  public void setLastName(String lastName) {
+    this.lastName = lastName;
+  }
+
+  public String getCaseNumber() {
+    return caseNumber;
+  }
+
+  public void setCaseNumber(String caseNumber) {
+    this.caseNumber = caseNumber;
+  }
+
+  public String getCity() {
+    return city;
+  }
+
+  public void setCity(String city) {
+    this.city = city;
+  }
+
+  public String getState() {
+    return state;
+  }
+
+  public void setState(String state) {
+    this.state = state;
+  }
+
+  public String getZip() {
+    return zip;
+  }
+
+  public void setZip(String zip) {
+    this.zip = zip;
+  }
+
+  public String getAddress() {
+    return address;
+  }
+
+  public void setAddress(String address) {
+    this.address = address;
+  }
+
+  public String getTelephone() {
+    return telephone;
+  }
+
+  public void setTelephone(String telephone) {
+    this.telephone = telephone;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+}
